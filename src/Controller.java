40d0d04783aeee691f9952576cdd4955637456dc
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-05-07 14:08:31 +0200

    comentarios

--- src/Controller.java
+++ src/Controller.java
@@ -3,6 +3,7 @@ import java.lang.module.ModuleDescriptor;
 public class Controller {
     /**
      * Crea el coche
+     * @return Valor de la matrícula del coche
      */
     public static String crearCoche(String modelo, String matricula) {
         //Usando los metodos del Model de manera estática, a diferencia que de la otra manera que los instanciábamos
